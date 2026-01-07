b71da8bd22e148a29d83e80a36946e9b9cacf607
Omer <omerfarkozkul@gmail.com>
2026-01-07 15:50:21 +0200

    Fixing tests

--- client-app/src/app/app.component.spec.ts
+++ client-app/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, NxWelcomeComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
   });
 
@@ -13,8 +16,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome client-app',
+    expect(compiled.querySelector('h2')?.textContent).toContain(
+      'Tasks',
     );
   });
 
--- client-app/src/app/components/add-task/add-task.component.spec.ts
+++ client-app/src/app/components/add-task/add-task.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddTaskComponent } from './add-task.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AddTaskComponent', () => {
   let component: AddTaskComponent;
@@ -8,6 +10,7 @@ describe('AddTaskComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddTaskComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddTaskComponent);
